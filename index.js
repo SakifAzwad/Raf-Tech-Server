eda1cd8d0799b569ada027c172c9d4bbc99445da
SakifAzwad <sakifazwad18@gmail.com>
2023-10-19 21:25:07 +0600

    Added GET to send Brands data

--- index.js
+++ index.js
@@ -1,18 +1,81 @@
-const express = require('express');
-const cors =require('cors');
-const app=express();
-require('dotenv').config();
-const port=process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>
-{
-    res.send("server running");
-})
+//raf-tech
+//zbaqF4dg78gRYx4u
 
-app.listen(port,()=>
-{
-    console.log(`Coffee server is running on port:${port}`);
-})
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://raf-tech:zbaqF4dg78gRYx4u@cluster0.gp9ypzc.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const raftechCol = client.db("raftech").collection("brand_name");
+
+    const raftechCol2 = client.db("raftech").collection("product_name");
+
+    app.get("/brandname", async (req, res) => {
+      const cursor = raftechCol.find();
+      const rest = await cursor.toArray();
+      res.send(rest);
+    });
+
+    app.get("/products", async (req, res) => {
+      const cursor = raftechCol2.find();
+      const rest = await cursor.toArray();
+      res.send(rest);
+    });
+
+    app.get("/products/:brname", async (req, res) => {
+        const brname = req.params.brname;
+        const brname2 = brname.toUpperCase();
+        const query = { brand_name: brname2 };
+        const result = await raftechCol2.find(query).toArray();
+        res.send(result);
+      });
+
+
+    app.get("/brandname/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await raftechCol.findOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server running");
+});
+
+app.listen(port, () => {
+  console.log(`Coffee server is running on port:${port}`);
+});
